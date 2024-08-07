5214ee1cdc2678cca5b794de34814bbbc891fa55
Nayla2102 <nayla_larasati_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:54:13 +0700

    Add files via upload

---
+++ Guru.java
@@ -0,0 +1,33 @@
+//Object Class
+public class Guru {
+    String Guru;
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    /*Constructor*/
+    public Guru() {
+        id = 1;
+        nama = "pakDiaur";
+        mapel = "pjok";
+        alamat = "malang";
+        id = 2;
+        nama = "buAulia";
+        mapel = "rpl produktif";
+        alamat = "malang";
+        id = 3;
+        nama = "buFeni";
+        mapel = "matematika";
+        alamat = "malang";
+        id = 4;
+        nama = "buPasha";
+        mapel = "rpl produktif";
+        alamat = "malang";
+        id = 5;
+        nama = "pakAndre";
+        mapel = "bahasa inggris";
+        alamat = "malang";
+    }
+    
+}
---
+++ TesGuru.java
@@ -0,0 +1,59 @@
+//Driver Class
+public class TesGuru {
+    public static void main(String[] args) {
+        //membuat objek
+        Guru pakDiaur = new Guru();
+        Guru buAulia = new Guru();
+        Guru buFeni = new Guru();
+        Guru buPasha = new Guru();
+        Guru pakAndre = new Guru();
+        pakDiaur.id = 1;
+        pakDiaur.nama = "pakDiaur";
+        pakDiaur.mapel = "pjok";
+        pakDiaur.alamat = "malang";
+        System.out.println("Ini Data Pak Diaur");
+        System.out.println(pakDiaur.id);
+        System.out.println(pakDiaur.nama);
+        System.out.println(pakDiaur.mapel);
+        System.out.println(pakDiaur.alamat);
+        buAulia.id = 2;
+        buAulia.nama = "buAulia";
+        buAulia.mapel = "rpl produktif";
+        buAulia.alamat = "malang";
+        System.out.println("Ini Data Bu Aulia");
+        System.out.println(buAulia.id);
+        System.out.println(buAulia.nama);
+        System.out.println(buAulia.mapel);
+        System.out.println(buAulia.alamat);
+        buFeni.id = 3;
+        buFeni.nama = "buFeni";
+        buFeni.mapel = "matematika";
+        buFeni.alamat = "malang";
+        System.out.println("Ini Data Bu Feni");
+        System.out.println(buFeni.id);
+        System.out.println(buFeni.nama);
+        System.out.println(buFeni.mapel);
+        System.out.println(buFeni.alamat);
+        buPasha.id = 4;
+        buPasha.nama = "buPasha";
+        buPasha.mapel = "rpl produktif";
+        buPasha.alamat = "malang";
+        System.out.println("Ini Data Bu Pasha");
+        System.out.println(buPasha.id);
+        System.out.println(buPasha.nama);
+        System.out.println(buPasha.mapel);
+        System.out.println(buPasha.alamat);
+        pakAndre.id = 5;
+        pakAndre.nama = "pakAndre";
+        pakAndre.mapel = "bahasa inggris";
+        pakAndre.alamat = "malang";
+        System.out.println("Ini Data Pak Andre");
+        System.out.println(pakAndre.id);
+        System.out.println(pakAndre.nama);
+        System.out.println(pakAndre.mapel);
+        System.out.println(pakAndre.alamat);
+
+    
+    }
+    
+}
