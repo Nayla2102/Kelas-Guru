97c798615af7ad3cc8a8670ad0bf4c0316f78eac
Nayla2102 <nayla_larasati_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 13:36:12 +0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,41 @@
+//Object Class
+public class Employee {
+    String name;
+    double salary;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    /*Constructor Employee*/
+    public Employee() {
+        salary = 0;
+        name = "";
+    }
+    //Constructor Parameter
+    public Employee(String name, double salary) {
+        this.name= name;
+        this.salary= salary;
+    }
+
+
+    //method
+    public void print() {
+        System.out.println("name :"+ name);
+        System.out.println("salary :"+ salary);
+
+    }
+
+}
